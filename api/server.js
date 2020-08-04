4442c2a5c01cb23ed608c7c849480b76b10dce2a
Daniel <dtues003@fiu.edu>
2020-08-03 20:21:47 -0400

    connected mongoDB

--- api/server.js
+++ api/server.js
@@ -1,27 +1,32 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-const cors = require("cors")
+const cors = require("cors");
+const connectDB = require("./config/db")
 
 dotenv.config({ path: "./config/config.env" });
 
-const app = express()
+connectDB();
 
-app.use(express.json())
+const app = express();
+
+app.use(express.json());
 
 if (process.env.NODE_ENV === "development") {
 	app.use(morgan("dev"));
 }
 
-app.use(cors())
+app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${process.env.PORT}`))
+const server = app.listen(
+	PORT,
+	console.log(`Server running in ${process.env.NODE_ENV} on port ${process.env.PORT}`)
+);
 
 process.on("unhandledRejection", (err, promise) => {
 	console.log(`Error: ${err.message}`);
 	// Close server & exit process
 	// server.close(() => process.exit(1));
 });
-
