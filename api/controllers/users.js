4e2be149c69f27ae6aa5d7741d876e088b6a9ba6
Daniel <dtues003@fiu.edu>
2020-08-15 17:55:18 -0400

    added user update functionality

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,14 +1,43 @@
-const asyncHandler = require("../middleware/async")
-const User = require("../models/User")
+const asyncHandler = require("../middleware/async");
+const User = require("../models/User");
 
 // @desc      Get all users
 // @route     GET /api/v1/users
 // @access    Private
 exports.getUsers = asyncHandler(async (req, res, next) => {
-   const users = await User.find();
-   res.status(200).json({
-      success: true,
-      length: users.length,
-      data: users
-   })
+	const users = await User.find();
+	res.status(200).json({
+		success: true,
+		length: users.length,
+		data: users,
+	});
+});
+
+// @desc      Get single users
+// @route     GET /api/v1/users/:id
+// @access    Private
+exports.getUser = asyncHandler(async (req, res, next) => {
+	const user = await User.findById(req.params.id);
+
+	res.status(200).json({
+		success: true,
+		data: user,
+	});
+});
+
+// @desc      Update user
+// @route     PUT /api/v1/users/
+// @access    Private
+exports.updateUser = asyncHandler(async (req, res, next) => {
+	const { name, description } = req.body;
+	const updateFields = { name, description };
+	const user = await User.findByIdAndUpdate(req.user.id, updateFields, {
+		new: true,
+		runValidators: true,
+	});
+
+	res.status(200).json({
+		success: true,
+		data: user,
+	});
 });
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const protect = require("../middleware/protect");
-const { getUsers } = require("../controllers/users");
+const { getUsers, getUser, updateUser } = require("../controllers/users");
 
 const router = express.Router();
 
 router.use(protect);
 
 router.get("/", getUsers);
+router.get("/:id", getUser)
+router.put('/', updateUser)
+
 
 module.exports = router;
